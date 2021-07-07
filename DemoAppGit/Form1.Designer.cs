b151af4d687522a2cb099191936473bee5b5dfa0
LEWIS OSWALDO LOPEZ GOMEZ <lopezsoft.com@gmail.com>
2021-07-06 22:34:52 -0500

    Otro boton

--- DemoAppGit/Form1.Designer.cs
+++ DemoAppGit/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace DemoAppGit
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,11 +43,21 @@ namespace DemoAppGit
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Hacer click";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 417);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@ namespace DemoAppGit
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
